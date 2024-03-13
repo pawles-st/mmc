05a60f3d891c828dd0f702a784d4c2430bb40039
pawles <268419@student.pwr.edu.pl>
2024-03-13 05:30:39 +0100

    Change title in graphs

--- list1/problem1/program.R
+++ list1/problem1/program.R
@@ -30,33 +30,33 @@ for (no_repeats in c(50, 500, 1000)) {
 	# plotting the numbers on the number line and drawing the frequency histogram
 
 	png(paste("generator1line_", no_repeats, ".png", sep = ""))
-	plot(random_values1, rep(0, no_values1), main = "Rozmieszczenie punktów z pierwszego generatora na odcinku", xlab = "wartość z odcinka", ylab = "")
+	plot(random_values1, rep(0, no_values1), main = paste("Rozmieszczenie punktów z pierwszego generatora na odcinku\n", no_repeats, " pierwszych liczb", sep = ""), xlab = "wartość z odcinka", ylab = "")
 	abline(h = 0)
 	dev.off()
 
 	png(paste("generator1hist_", no_repeats, ".png", sep = ""))
-	hist(random_values1, breaks = 10, probability = TRUE, main = "Histogram pierwszego generatora", xlab = "zakresy", ylab = "prawdopodobieństwo")
+	hist(random_values1, breaks = 10, probability = TRUE, main = paste("Histogram pierwszego generatora\n", no_repeats, " pierwszych liczb", sep = ""), xlab = "zakresy", ylab = "prawdopodobieństwo")
 	abline(h = 1, col = "green", lwd = 3)
 	dev.off()
 
 	png(paste("generator2line_", no_repeats, ".png", sep = ""))
-	plot(random_values2, rep(0, no_values2), main = "Rozmieszczenie punktów z drugiego generatora na odcinku", xlab = "wartość z odcinka", ylab = "")
+	plot(random_values2, rep(0, no_values2), main = paste("Rozmieszczenie punktów z drugiego generatora na odcinku\n", no_repeats, " pierwszych liczb", sep = ""), xlab = "wartość z odcinka", ylab = "")
 	abline(h = 0)
 	dev.off()
 
 	png(paste("generator2hist_", no_repeats, ".png", sep = ""))
-	hist(random_values2, breaks = 10, probability = TRUE, main = "Histogram drugiego generatora", xlab = "zakresy", ylab = "prawdopodobieństwo")
+	hist(random_values2, breaks = 10, probability = TRUE, main = paste("Histogram drugiego generatora\n", no_repeats, " pierwszych liczb", sep = ""), xlab = "zakresy", ylab = "prawdopodobieństwo")
 	abline(h = 1, col = "green", lwd = 3)
 	dev.off()
 
 	# plotting the number pairs from one generator on the square to visualise dependency
 
 	png(paste("dependent1_", no_repeats, ".png", sep = ""))
-	plot(random_values1[2 * 1:(no_values1 / 2) - 1], random_values1[2 * 1:(no_values1 / 2)], main = "Rozmieszczenie punktów z pierwszego generatora na kwadracie", xlab = "pierwsza współrzędna", ylab = "druga współrzędna")
+	plot(random_values1[2 * 1:(no_values1 / 2) - 1], random_values1[2 * 1:(no_values1 / 2)], main = paste("Rozmieszczenie punktów z pierwszego generatora na kwadracie\n", no_repeats, " pierwszych liczb", sep = ""), xlab = "pierwsza współrzędna", ylab = "druga współrzędna")
 	dev.off()
 
 	png(paste("dependent2_", no_repeats, ".png", sep = ""))
-	plot(random_values2[2 * 1:(no_values2 / 2) - 1], random_values2[2 * 1:(no_values2 / 2)], main = "Rozmieszczenie punktów z drugiego generatora na kwadracie", xlab = "pierwsza współrzędna", ylab = "druga współrzędna")
+	plot(random_values2[2 * 1:(no_values2 / 2) - 1], random_values2[2 * 1:(no_values2 / 2)], main = paste("Rozmieszczenie punktów z drugiego generatora na kwadracie\n", no_repeats, " pierwszych liczb", sep = ""), xlab = "pierwsza współrzędna", ylab = "druga współrzędna")
 	dev.off()
 
 	# plotting the number pairs from two used generators
