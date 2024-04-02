83558c7682aafeffdb170d5a2ef6e1d62cc11012
pawles <268419@student.pwr.edu.pl>
2024-04-02 22:03:34 +0200

    Add file saving for problem2 in list2

--- list2/problem2/solution.R
+++ list2/problem2/solution.R
@@ -6,19 +6,29 @@ rand.norm2 <- sqrt(-2 * log(rand.unif2)) * sin(2 * pi * rand.unif1)
 
 # normal distribution
 
+jpeg("normal.jpeg")
 hist(rand.norm1, prob = TRUE, breaks = 30)
 pts <- seq(-10, 10, by = 0.01)
 norm.density <- dnorm(pts)
 lines(pts, norm.density, col = 2, lwd = 3)
+dev.off()
 
 # normal combination
 
 rand.norm1 <- rand.norm1[1:1000]
 rand.norm2 <- rand.norm2[1:1000]
 
+jpeg("multi1.jpeg")
 plot(rand.norm1, rand.norm2)
+dev.off()
+
+jpeg("multi2.jpeg")
 plot(rand.norm1, rand.norm1 + rand.norm2)
+dev.off()
+
+jpeg("multi3.jpeg")
 plot((rand.norm1 + rand.norm2) / sqrt(2), (rand.norm1 - rand.norm2) / sqrt(2))
+dev.off()
 
 
 
