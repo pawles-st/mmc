d27206d9f5fd105d5840b281000a3f4b5b8d0b7d
pawles <268419@student.pwr.edu.pl>
2024-04-03 06:01:39 +0200

    Solve problem3 from list2

---
+++ list2/problem3/solution.R
@@ -0,0 +1,13 @@
+rand.unif1 <- runif(10000)
+rand.unif2 <- runif(10000)
+
+rand.abs.norm <- sqrt(-2 * log(rand.unif2)) * abs(cos(2 * pi * rand.unif1))
+
+# absolute normal
+
+jpeg("abs_normal.jpeg")
+hist(rand.abs.norm, prob = TRUE, breaks = 30)
+pts <- seq(0, 10, by = 0.01)
+abs.norm.density <- 2 * dnorm(pts)
+lines(pts, abs.norm.density, col = 2, lwd = 3)
+dev.off()
